1472edad0e7d44bcad2983ab2bbfacd90c5ed084
rancobc <ran@broadcust.com>
2021-02-24 19:50:41 +0200

    comment out use of aws secret

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const moment = require('moment');
 
-const app = express()
+const app = express();
 const AWS = require('aws-sdk');
 const request = require('request');
 const cgDataToEZC = require('./utils');
@@ -19,47 +19,47 @@ let   secret;
 let   decodedBinarySecret;
 
 // Create a Secrets Manager client
-const client = new AWS.SecretsManager({
-    region: region
-});
+// const client = new AWS.SecretsManager({
+//     region: region
+// });
 
-client.getSecretValue({SecretId: secretName}, function(err, data) {
-    if (err) {
-        if (err.code === 'DecryptionFailureException')
-        // Secrets Manager can't decrypt the protected secret text using the provided KMS key.
-        // Deal with the exception here, and/or rethrow at your discretion.
-            throw err;
-        else if (err.code === 'InternalServiceErrorException')
-        // An error occurred on the server side.
-        // Deal with the exception here, and/or rethrow at your discretion.
-            throw err;
-        else if (err.code === 'InvalidParameterException')
-        // You provided an invalid value for a parameter.
-        // Deal with the exception here, and/or rethrow at your discretion.
-            throw err;
-        else if (err.code === 'InvalidRequestException')
-        // You provided a parameter value that is not valid for the current state of the resource.
-        // Deal with the exception here, and/or rethrow at your discretion.
-            throw err;
-        else if (err.code === 'ResourceNotFoundException')
-        // We can't find the resource that you asked for.
-        // Deal with the exception here, and/or rethrow at your discretion.
-            throw err;
-    }
-    else {
-        // Decrypts secret using the associated KMS CMK.
-        // Depending on whether the secret is a string or binary, one of these fields will be populated.
-        if ('SecretString' in data) {
-            secret = data.SecretString;
-        } else {
-            let buff = new Buffer(data.SecretBinary, 'base64');
-            decodedBinarySecret = buff.toString('ascii');
-        }
-    }
-
-    // Your code goes here.
-});
-
+// client.getSecretValue({SecretId: secretName}, function(err, data) {
+//     if (err) {
+//         if (err.code === 'DecryptionFailureException')
+//         // Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+//         // Deal with the exception here, and/or rethrow at your discretion.
+//             throw err;
+//         else if (err.code === 'InternalServiceErrorException')
+//         // An error occurred on the server side.
+//         // Deal with the exception here, and/or rethrow at your discretion.
+//             throw err;
+//         else if (err.code === 'InvalidParameterException')
+//         // You provided an invalid value for a parameter.
+//         // Deal with the exception here, and/or rethrow at your discretion.
+//             throw err;
+//         else if (err.code === 'InvalidRequestException')
+//         // You provided a parameter value that is not valid for the current state of the resource.
+//         // Deal with the exception here, and/or rethrow at your discretion.
+//             throw err;
+//         else if (err.code === 'ResourceNotFoundException')
+//         // We can't find the resource that you asked for.
+//         // Deal with the exception here, and/or rethrow at your discretion.
+//             throw err;
+//     }
+//     else {
+//         // Decrypts secret using the associated KMS CMK.
+//         // Depending on whether the secret is a string or binary, one of these fields will be populated.
+//         if ('SecretString' in data) {
+//             secret = data.SecretString;
+//         } else {
+//             let buff = new Buffer(data.SecretBinary, 'base64');
+//             decodedBinarySecret = buff.toString('ascii');
+//         }
+//     }
+//
+//     // Your code goes here.
+// });
+//
 
 
 app.use(bodyParser.json({ strict: false }));
